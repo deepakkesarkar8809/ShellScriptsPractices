33a968f0d7f02c64400f69a064abce8a43bb06ef
Deepak Kesarkar <deeps9427@gmail.com>
2025-04-16 21:06:36 +0530

    Day 1 Completed Successfully

---
+++ Day-1/day_01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## We are now following the standard method of creating scripts using Bash.
+## Earlier, we used to create scripts without a shebang line, which executed them via 'sh'.
+## Now, scripts will include the shebang line and execute through '/bin/bash'.
+
+echo "this is our standard script using the bin/bash sebang line"
+echo "exucuted successfully"
+
+date
+pwd 
+
+
+sleep 10
