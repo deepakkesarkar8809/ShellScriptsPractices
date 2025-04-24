1c4da144b2fb6c28129bdf4029fd9991d774bbfb
Deepak Kesarkar <deeps9427@gmail.com>
2025-04-25 00:39:00 +0530

    Added the new scripts

---
+++ Day-7/variable_name.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Valid variable names in shell scripts
+valid_name1="Hello"
+valid_name2="World"
+_valid_name3="Shell"
+valid_name_4="Scripting"
+validName5="Practices"
+
+# Displaying the values of valid variables
+echo $valid_name1
+echo $valid_name2
+echo $_valid_name3
+echo $valid_name_4
+echo $validName5
+
+name="Deepak"
+NaMe="Vishal"
+NAME="Pratik Patil"
+
+echo $name $NaMe $NAME  
+
+## All the above variables indicate that they are different variables and case sensitive
+
+## if we start using the variable name with a number, it will throw an error
---
+++ Day-8/system_var.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script: system_variables.sh
+# Purpose: Demonstrate system variables usage.
+echo "Below few are the some of the system variables in bash."
+
+echo "User: $USER"
+echo "Home Directory: $HOME"
+echo "Shell: $SHELL"
+echo "Machine Name: $HOSTNAME"
+echo "Working Directory: $PWD"
+echo "User ID: $UID"
+echo "System Path: $PATH"
+echo "Operating System: $OSTYPE"
+echo "Parent Process ID: $PPID"
+echo "Script Runtime: $SECONDS seconds"
+echo "Random Number: $RANDOM"
+echo "Terminal Dimensions: $LINES lines x $COLUMNS columns"
+echo "Temporary Directory: $TMPDIR"
