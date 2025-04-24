0d02768ed4b8c3a6b5f7ca00286816ab7e522b6d
Deepak Kesarkar <deeps9427@gmail.com>
2025-04-24 19:09:29 +0530

    Day-6 has been completed

---
+++ Day-6/Variables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## In the below script we learnt how we can define or use the variables
+## User define variables
+
+name="Vishal" 
+age="29"
+
+## System define variables
+
+echo ${name}
+
+echo "My name is ${name} and my age is ${age}" 
+
+work="study"
+var="ing"
+
+echo " I am ${work}${var}" ## Conacatination of two variables
